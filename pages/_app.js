a2086e2436272384dc90ca6bc25764660229f6da
BusraFersadoglu <busra.fersadoglu@gmail.com>
2023-01-12 12:48:30 +0300

    Add new state

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
+import { useState } from "react";
 import "../styles/globals.css";
 import Header from "../components/header";
 
 export default function App({ Component, pageProps }) {
+  const [theme, setTheme] = useState("Light");
   return (
-    <>
-      <Header />
+    <div className={theme}>
+      <Header theme={theme} setTheme={setTheme} />
       <Component {...pageProps} />
-    </>
+    </div>
   );
 }
