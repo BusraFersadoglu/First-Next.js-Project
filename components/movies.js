975ad08f96a49e828f8c6c0a8df798d20107c34e
BusraFersadoglu <busra.fersadoglu@gmail.com>
2023-01-12 12:47:16 +0300

    Delete Jsx style

--- components/movies.js
+++ components/movies.js
@@ -4,20 +4,14 @@ export default function Movies({ movies }) {
   const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
   return (
     <div className="movies">
-      <h1>Popular Movies</h1>
       {movies.results.map((movie, index) => (
         <div key={index}>
           <Link href={`/movie/${movie.id}`}>
-            <p>{movie.original_title}</p>
-            <img src={`${IMG_PATH}${movie.poster_path}`} />
+            <img src={`${IMG_PATH}${movie.poster_path}`} id="movies-poster" />
+            <p id="movies-title">{movie.title}</p>
           </Link>
         </div>
       ))}
-      <style jsx>{`
-        img {
-          width: 15rem;
-        }
-      `}</style>
     </div>
   );
 }
